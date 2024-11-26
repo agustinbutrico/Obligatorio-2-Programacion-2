292dbfe646a7d10779150ab837fc7c6f09b13102
Agus_But <agustinbutrico@gmail.com>
2024-11-26 11:33:28 -0300

    Se personalizó el Login y Register.

--- Proyecto/LogicaNegocio/obj/Debug/net8.0/LogicaNegocio.AssemblyInfo.cs
+++ Proyecto/LogicaNegocio/obj/Debug/net8.0/LogicaNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd968848eac49ecf31e64c61bd0c15e57987beb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a24b8a2ed580ae6ec70aff293f57407b13c6e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
