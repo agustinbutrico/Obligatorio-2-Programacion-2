bd968848eac49ecf31e64c61bd0c15e57987beb6
Agus_But <agustinbutrico@gmail.com>
2024-11-25 21:22:58 -0300

    Se configuró el metodo get y post en account para Login y Register

--- Proyecto/InterfazUsuario/Controllers/AccountController.cs
+++ Proyecto/InterfazUsuario/Controllers/AccountController.cs
@@ -4,12 +4,25 @@ namespace InterfazUsuario.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Login(string email, string contrasenia)
+        {
+            return View();
+        }
 
+        [HttpGet]
         public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string name, string apellido, string email, string contrasenia)
         {
             return View();
         }
