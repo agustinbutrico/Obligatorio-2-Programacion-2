333c1ceac11310925959e9d8f8b66114ebce8c31
Agus_But <agustinbutrico@gmail.com>
2024-11-28 11:26:45 -0300

    Se actualizaron las precargas.

--- Proyecto/LogicaNegocio/obj/Debug/net8.0/LogicaNegocio.AssemblyInfo.cs
+++ Proyecto/LogicaNegocio/obj/Debug/net8.0/LogicaNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09260644232d059e90277bcce3d04872635f11ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9109fa2e497afe0b5572081facd93f3596ff609f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
